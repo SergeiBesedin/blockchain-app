b33e0abb082565542b822589bf75f5d8e229f498
SergeiBesedin <sergey-besedin98@mail.ru>
2022-09-05 21:03:02 +0300

    реализовал логику добавления нового блока

--- src/browser/main.ts
+++ src/browser/main.ts
@@ -8,7 +8,7 @@ const enum Status {
 }
 
 class Main {
-  //   statusEl: HTMLElement;
+  statusEl: HTMLElement;
   blocksEl: HTMLElement;
   pendingListEl: HTMLElement;
   confirmBtn: HTMLButtonElement;
@@ -20,47 +20,69 @@ class Main {
 
   constructor() {
     this.blockchain = new Blockchain();
+    this.statusEl = document.getElementById('status');
     this.blocksEl = document.getElementById('blocks');
-    this.pendingListEl = document.getElementById('pendign-list');
+    this.pendingListEl = document.getElementById('pending-list');
     this.confirmBtn = document.getElementById('confirm') as HTMLButtonElement;
     this.transferBtn = document.getElementById('transfer') as HTMLButtonElement;
     this.senderInp = document.getElementById('sender') as HTMLInputElement;
     this.recipientInp = document.getElementById(
       'recipient'
     ) as HTMLInputElement;
-    this.amountInp = document.getElementById('amount') as HTMLInputElement;
+    this.amountInp = document.getElementById('number') as HTMLInputElement;
     this.confirmBtn.onclick = this._mineBlock.bind(this);
     this.transferBtn.onclick = this._addNewTransaction.bind(this);
   }
 
   private _clearForm(): void {
-    this.senderInp.innerHTML = '';
-    this.recipientInp.innerHTML = '';
-    this.amountInp.innerHTML = '';
+    this.senderInp.value = '';
+    this.recipientInp.value = '';
+    this.amountInp.value = '';
   }
 
   async addGenesisBlock() {
+    this.statusEl.textContent = Status.Initialization;
     await this.blockchain.createGenesisBlock();
     this.blocksEl.innerHTML = this.blockchain.chain
       .map((block, ind) => this._createBlockHtml(block, ind))
       .join('');
+    // this.statusEl.textContent = Status.AddTransaction;
+    this._toggleState(true, false);
   }
 
   private _addNewTransaction(): void {
-    const payload = {
+    const transaction = {
       sender: this.senderInp.value,
       recipient: this.recipientInp.value,
       amount: Number(this.amountInp.value),
     };
-    this.blockchain.createTransaction(payload);
+    this._toggleState(false, false);
+    this.blockchain.createTransaction(transaction);
+    const pendingItem = document.createElement('li');
+    pendingItem.classList.add('pending-item');
+    pendingItem.textContent = `${transaction.sender} — ${transaction.recipient}: $${transaction.amount}`;
+    this.pendingListEl.appendChild(pendingItem);
+    this.statusEl.textContent = Status.ReadyToMine;
     this._clearForm();
   }
 
   private async _mineBlock() {
+    this.statusEl.textContent = Status.MineInProgress;
+    this._toggleState(true, true);
     await this.blockchain.minePendignTransaction();
+    this.blocksEl.innerHTML = this.blockchain.chain
+      .map((block, ind) => this._createBlockHtml(block, ind))
+      .join('');
+    this._toggleState(true, false);
+    this.pendingListEl.innerHTML = '';
   }
 
   private _toggleState(confirm: boolean, transferForm: boolean): void {
+    this.transferBtn.disabled =
+      this.amountInp.disabled =
+      this.senderInp.disabled =
+      this.recipientInp.disabled =
+        transferForm;
     this.confirmBtn.disabled = confirm;
   }
 
